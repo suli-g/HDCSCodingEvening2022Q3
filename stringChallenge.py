6c43285fdf3b12389a3b3835c90d7ec120046824
Sulaiman <suligzus@gmail.com>
2022-08-04 19:26:38 +0200

    Fix logical error

--- stringChallenge.py
+++ stringChallenge.py
@@ -1,3 +1,5 @@
+from typing import List, Any
+
 """ Gets a list of specific properties for a given string.
 
 Returns:
@@ -9,18 +11,17 @@ Returns:
             - "@ index x" if the second character reoccurs, x = index of second character.
             - "not found" if the second character does not reoccur.
 """
-from typing import List, Any
-
 def get_string_properties(given_string: str) -> List[Any]:
     string_length: int = len(given_string)
     middle_index: int = int(string_length/2)
-    second_char_repeats = given_string[1:].find(given_string[1])
+    second_char_repeats = given_string[2:].find(given_string[1])
+
     return [
         string_length,
         given_string[-1],
         # Uses the fact that int(False) == 0 and int(True) == 1 in python.
         given_string[middle_index: middle_index + 1 + int(string_length % 2 != 0)],
-        f"@ index {second_char_repeats}" if second_char_repeats > - 1 else "not found"
+        f"@ index {second_char_repeats + 1}" if second_char_repeats > - 1 else "not found"
     ]
 
 print(get_string_properties("seven"))
